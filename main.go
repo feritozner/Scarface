c0b680e7f006de4aec491f705582c2b12706bdde
FeZq <feritozner@gmail.com>
2025-06-08 00:19:33 +0300

    Scarface version 1.2 (cli update)

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"strings"
 )
 
 const (
@@ -21,14 +22,24 @@ var status string
 func main() {
 	// Command Line Flags
 	data := flag.String("data", "", "Data to encode/decode")
-	toBase64 := flag.Bool("to_base64", false, "Conver to Base64")
-	fromBase64 := flag.Bool("from_base64", false, "Convert from Base64")
-	toHex := flag.Bool("to_hex", false, "Convert to Hex")
-	fromHex := flag.Bool("from_hex", false, "Convert from Hex")
-	toURL := flag.Bool("to_url", false, "Convert to Url encode")
-	fromURL := flag.Bool("from_url", false, "Convert from Url encode")
+	tb64 := flag.Bool("tb64", false, "Convert to Base64")
+	fb64 := flag.Bool("fb64", false, "Convert from Base64")
+	th := flag.Bool("th", false, "Convert to Hex")
+	fh := flag.Bool("fh", false, "Convert from Hex")
+	tu := flag.Bool("tu", false, "Convert to URL encode")
+	fu := flag.Bool("fu", false, "Convert from URL encode")
+	h := flag.Bool("h", false, "Show help message")
+	tascii := flag.Bool("tasc", false, "Convert to ASCII")
+	fascii := flag.Bool("fasc", false, "Convert from ASCII")
+	help := flag.Bool("help", false, "Show help message")
 
 	flag.Parse()
+
+	if *h || *help {
+		PrintHelp()
+		return
+	}
+
 	if *data == "" {
 		status = "No data provided"
 		BannerStart()
@@ -36,29 +47,28 @@ func main() {
 		BannerEnd()
 		os.Exit(1)
 	}
+
 	switch {
-	case *toBase64:
+	case *tb64:
 		status = "Text to Base64"
 		BannerStart()
 		Result(*data, EncodeBase64(*data))
 
-	case *fromBase64:
-
+	case *fb64:
 		result, err := DecodeBase64(*data)
 		if err != nil {
 			fmt.Println(Red + "Base64 decode error: " + err.Error() + Reset)
 			BannerEnd()
 			os.Exit(1)
 		}
-		fmt.Println(Green + result + Reset)
+		Result(*data, result)
 
-	case *toHex:
-
+	case *th:
 		status = "Text to Hex"
 		BannerStart()
 		Result(*data, EncodeHex(*data))
 
-	case *fromHex:
+	case *fh:
 		result, err := DecodeHex(*data)
 		if err != nil {
 			fmt.Println(Red + "Hex decode error: " + err.Error() + Reset)
@@ -67,15 +77,31 @@ func main() {
 		}
 		Result(*data, result)
 
-	case *toURL:
+	case *tu:
 		status = "Text to URL Encode"
 		BannerStart()
-		Result(*data, EncodeBase64(*data))
+		Result(*data, EncodeURL(*data))
 
-	case *fromURL:
+	case *fu:
 		result, err := DecodeURL(*data)
 		if err != nil {
 			fmt.Println(Red + "URL decode error: " + err.Error() + Reset)
+			BannerEnd()
+			os.Exit(1)
+		}
+		Result(*data, result)
+
+	case *tascii:
+		status = "Text to ASCII"
+		BannerStart()
+		Result(*data, EncodeASCII(*data))
+
+	case *fascii:
+		status = "ASCII to Text"
+		BannerStart()
+		result, err := DecodeASCII(*data)
+		if err != nil {
+			fmt.Println(Red + "ASCII decode error: " + err.Error() + Reset)
 			BannerEnd()
 			os.Exit(1)
 		}
@@ -84,7 +110,7 @@ func main() {
 		status = "No valid operation selected"
 		BannerStart()
 		fmt.Println(Red + "Error: " + Reset + "No valid operation specified. Use one of the flags: " +
-			Green + "-to_base64, -from_base64, -to_hex, -from_hex, -to_url, -from_url" + Reset)
+			Green + "-tb64, -fb64, -th, -fh, -tu, -fu" + Reset)
 		BannerEnd()
 		os.Exit(1)
 	}
@@ -92,11 +118,57 @@ func main() {
 	BannerEnd()
 }
 
+func PrintHelp() {
+	status = "Help Menu"
+	BannerStart()
+	fmt.Println(Green + "Scarface Encoder/Decoder Tool" + Reset)
+	fmt.Println(Cyan + "Usage:" + Reset)
+	fmt.Println("  " + Green + "scarface" + Reset + " [flags]")
+	fmt.Println()
+	fmt.Println(Cyan + "Flags:" + Reset)
+	fmt.Println(Green + "  -data" + Reset + "           Input data to encode/decode")
+	fmt.Println(Green + "  -tb64" + Reset + "           Encode input to Base64")
+	fmt.Println(Green + "  -fb64" + Reset + "           Decode input from Base64")
+	fmt.Println(Green + "  -th" + Reset + "             Encode input to Hex")
+	fmt.Println(Green + "  -fh" + Reset + "             Decode input from Hex")
+	fmt.Println(Green + "  -tu" + Reset + "             Encode input to URL")
+	fmt.Println(Green + "  -fu" + Reset + "             Decode input from URL")
+	fmt.Println(Green + "  -h" + Reset + "              Show this help message")
+	fmt.Println(Green + "  -help" + Reset + "           Show this help message")
+	fmt.Println()
+	BannerEnd()
+}
+
 func Result(before string, after string) {
 
-	fmt.Println(Cyan + "Input: " + Reset + before)
+	fmt.Println(Cyan + "Input: \n\t" + Reset + before)
 	fmt.Println(Cyan + "------------------")
-	fmt.Println("Output: " + Reset + after)
+	fmt.Println("Output: \n\t" + Reset + after)
+}
+
+// ASCII encode/decode
+func EncodeASCII(input string) string {
+	var result string
+	for i, c := range input {
+		if i > 0 {
+			result += " "
+		}
+		result += fmt.Sprintf("%d", c)
+	}
+	return result
+}
+
+func DecodeASCII(input string) (string, error) {
+	var result string
+	var code int
+	for _, s := range strings.Fields(input) {
+		_, err := fmt.Sscanf(s, "%d", &code)
+		if err != nil {
+			return "", fmt.Errorf("invalid ASCII code: %s", s)
+		}
+		result += string(rune(code))
+	}
+	return result, nil
 }
 
 // Base64 encode/decode
@@ -132,7 +204,7 @@ func DecodeURL(input string) (string, error) {
 func BannerStart() {
 
 	fmt.Println(Red + "------------------------------------------------------------  " + Reset)
-	fmt.Println(Red + "Scarface # " + Green + "Version 1.0 # " + Cyan + status + " #")
+	fmt.Println(Red + "Scarface # " + Green + "Version 1.2 # " + Cyan + status + " #")
 	fmt.Println(Red + "------------------------------------------------------------  " + Reset)
 
 }
